881350da4c85e473b4912f312139d899db055c3c
Mathijs Nabbe <datenenaampje@gmail.com>
2023-10-18 14:09:06 +0200

    Read file & folder count

--- WindowsFileUtils/Services/SubfolderUnpacker.cs
+++ WindowsFileUtils/Services/SubfolderUnpacker.cs
@@ -2,6 +2,10 @@
 
 public class SubfolderUnpacker
 {
+    private int _fileCount = 0;
+    private int _folderCount = 0;
+    private int _errorCount = 0;
+
     public void Run()
     {
         Console.Clear();
@@ -14,8 +18,34 @@ public class SubfolderUnpacker
         Console.ForegroundColor = ConsoleColor.Green;
         var rootDirectory = Path.GetFullPath(Console.ReadLine() ?? string.Empty);
         Console.ResetColor();
+        Console.WriteLine();
 
-        Console.Clear();
+        Console.WriteLine("Scanning Directories");
+        CountFilesAndFolders(rootDirectory);
+        Thread.Sleep(1000);
+        var foundText = $"Found {_fileCount} files in {_folderCount} folders.";
+        if (_errorCount > 0)
+            foundText += $" {_errorCount} folders were inaccessible";
+        Console.WriteLine(foundText);
+        Console.WriteLine();
+        Console.WriteLine("Press enter to continue.");
+        Console.ReadLine();
+    }
 
+    private void CountFilesAndFolders(string directory)
+    {
+        try
+        {
+            _folderCount++;
+            _fileCount += Directory.GetFiles(directory).Length;
+
+            string[] subdirectories = Directory.GetDirectories(directory);
+            foreach (string folder in subdirectories)
+                CountFilesAndFolders(folder);
+        }
+        catch (UnauthorizedAccessException)
+        {
+            _errorCount++;
+        }
     }
 }
